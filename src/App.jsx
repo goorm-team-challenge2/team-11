31285013fe25f321276176510c2f00686eb0961d
임원호 <kds0034@gmail.com>
2023-09-23 06:11:58 +0000

    feat:  모달 3번 뼈대

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { EmptyView, Header } from '@/components';
+import { EmptyView, Header, Modal } from '@/components';
 
 import styles from './App.module.scss';
 
 function App() {
 	return (
 		<div className={styles.App}>
-			<Header />
-			<main className={styles.main}>
-				<EmptyView />
-			</main>
+			<Modal />
 		</div>
 	);
 }
